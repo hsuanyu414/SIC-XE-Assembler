083f5968efc1d32a11fd62136deedcd1e5827897
HsuanYu <93172849+hsuanyu414@users.noreply.github.com>
2022-01-21 00:57:37 +0800

    pass1 1.1

--- dist/main.py
+++ dist/main.py
@@ -33,12 +33,16 @@ SYMTAB = {}
 starting_address = 0
 program_length = 0 
 EXTEND = 0
+indirect = 0
+immediate = 0
+index = 0
+
 ofd = None
 
 def intToHexStr(number):
 	hexStr = hex(number)
 	hexStr = hexStr[2:]
-	while(len(hexStr)<4):
+	while(len(hexStr)<6):
 		hexStr = '0'+hexStr
 	return hexStr
 
@@ -67,6 +71,27 @@ def is_blankLine(instruction):
 			return False
 	return True
 
+def in_SYMTAB(element):
+	global indirect
+	global immediate
+	global index
+	indirect = 0
+	immediate = 0
+	index = 0
+	if(element in SYMTAB):
+		return True
+	elif(len(element)>= 2 and element[0] == '@' and element[1:] in SYMTAB):
+		indirect = 1
+		return True
+	elif(len(element)>= 2 and element[0] == '#' and element[1:] in SYMTAB):
+		immediate = 1
+		return True
+	elif(element[:-2] in SYMTAB and element[-2:] == ",X"):
+		index = 1
+		return True
+	else:
+		return False
+
 def in_OPTAB(element):
 	global EXTEND
 	EXTEND = 0
@@ -86,6 +111,7 @@ def pass1(inputFilePath, outputFilePath):
 		ofd = sys.stdout
 	# opening the input file and output file's fd
 	LOCCTR = 0
+	last = 0
 	count = 0
 	duplicate_symbol = 0
 	label_found = 0
@@ -105,7 +131,7 @@ def pass1(inputFilePath, outputFilePath):
 				instruction.insert(0, '')
 			else:
 				print('label with no definition.')
-				return
+				return -1
 		if(len(instruction)==2):
 			if(in_OPTAB(instruction[0])):#====== If the first element is in OPTAB, append ' ' at index 0
 				instruction.insert(0, '')
@@ -117,8 +143,9 @@ def pass1(inputFilePath, outputFilePath):
 		OPERAND = instruction[3]
 		if(count == 0):
 			if(OPCODE == 'START'):
-				LOCCTR = int(OPERAND)
+				LOCCTR = int(OPERAND, 16)
 				starting_address = LOCCTR
+				last = LOCCTR
 				printline(LOCCTR, LABEL, OPCODE, OPERAND)
 				continue
 		if(OPCODE == 'END'):
@@ -128,13 +155,13 @@ def pass1(inputFilePath, outputFilePath):
 			if(len(LABEL)!=0):#if there is a symbol in the LABEL field
 				if(LABEL in SYMTAB):#if found
 					print('duplicate symbol')
-					return
+					return -1
 				elif(LABEL == 'WORD' or LABEL == 'RESW' or LABEL == 'RESB' or LABEL == 'BYTE' or LABEL == 'BASE'):
 					print('assembler driective as symble')
-					return
+					return -1
 				elif(in_OPTAB(LABEL)):
 					print('opcode as label')
-					return
+					return -1
 				else:
 					SYMTAB[LABEL] = LOCCTR
 				#search OPTAB for OPCODE
@@ -143,7 +170,7 @@ def pass1(inputFilePath, outputFilePath):
 					FORMAT = (OPTAB[OPCODE[1:]]['FORMAT'])
 					if(FORMAT != 3):
 						print('extend format without FORMAT 3')
-						return
+						return -1 
 					LOCCTR += (FORMAT+1)
 				else:
 					FORMAT = (OPTAB[OPCODE]['FORMAT'])
@@ -163,16 +190,18 @@ def pass1(inputFilePath, outputFilePath):
 				LOCCTR += 0
 			else:
 				print('invalid operation code')
-				return
+				return -1
 		# print(SYMTAB)
-		printline(LOCCTR, LABEL, OPCODE, OPERAND)
+		printline(last, LABEL, OPCODE, OPERAND)
+		last = LOCCTR
 		count += 1
 	printSYMTAB(SYMTAB)
+	global program_length
 	program_length = LOCCTR - starting_address
 	print('program_length = '+ intToHexStr(program_length))
-	return 
+	return 1
 
-def pass2(fd, target):
+def pass2(inputFilePath, outputFilePath):
 	fd = open(current_folder+'/'+inputFilePath, 'r', encoding='utf-8')
 	if(outputFilePath):
 		ofd = open(current_folder+'/'+outputFilePath, 'w', encoding='utf-8')
@@ -182,8 +211,12 @@ def pass2(fd, target):
 	LOCCTR = 0
 	count = 0
 	duplicate_symbol = 0
+	X = 0
+	t_record = "T "
+	t_record_len = 0
+	global program_length
 	label_found = 0
-	print('LOC\tLABEL\tSTATEMENT\t\tOBJCODE')
+	# print('LOC\tLABEL\tSTATEMENT\t\tOBJCODE')
 	lines = fd.readlines()
 	for line in lines:
 		line = line.rstrip("\n")
@@ -213,64 +246,34 @@ def pass2(fd, target):
 			if(OPCODE == 'START'):
 				LOCCTR = int(OPERAND)
 				starting_address = LOCCTR
-				printline(LOCCTR, LABEL, OPCODE, OPERAND)
+				while(len(LABEL)<6):
+					LABEL = LABEL+' '
+				print("H " + LABEL + ' '+ intToHexStr(LOCCTR) + ' ' +intToHexStr(program_length))
 				continue
 		if(OPCODE == 'END'):
-			printline(LOCCTR, LABEL, OPCODE, OPERAND)
+			print("E " + intToHexStr(starting_address))
 			break
 		else:#if this is not a comment line
-			if(len(LABEL)!=0):#if there is a symbol in the LABEL field
-				if(LABEL in SYMTAB):#if found
-					print('duplicate symbol')
-					return
-				elif(LABEL == 'WORD' or LABEL == 'RESW' or LABEL == 'RESB' or LABEL == 'BYTE' or LABEL == 'BASE'):
-					print('assembler driective as symble')
-					return
-				elif(in_OPTAB(LABEL)):
-					print('opcode as label')
-					return
-				else:
-					SYMTAB[LABEL] = LOCCTR
-				#search OPTAB for OPCODE
+			#29個 byte 換
 			if(in_OPTAB(OPCODE)):
-				if(EXTEND):
-					FORMAT = (OPTAB[OPCODE[1:]]['FORMAT'])
-					if(FORMAT != 3):
-						print('extend format without FORMAT 3')
-						return
-					LOCCTR += (FORMAT+1)
-				else:
-					FORMAT = (OPTAB[OPCODE]['FORMAT'])
-					LOCCTR += FORMAT	
-			elif(OPCODE == 'WORD'):
-				LOCCTR += 3
-			elif(OPCODE == 'RESW'):
-				LOCCTR += 3*int(OPERAND)
-			elif(OPCODE == 'RESB'):
-				LOCCTR += 1*int(OPERAND)
-			elif(OPCODE == 'BYTE'):
-				if(OPERAND[0] == 'C'):
-					LOCCTR += len(OPERAND[2:-1])
-				elif(OPERAND[0] == 'X'):
-					LOCCTR += len(OPERAND[2:-1])//2
-			elif(OPCODE == 'BASE'):
-				LOCCTR += 0
-			else:
-				print('invalid operation code')
-				return
-		# print(SYMTAB)
-		printline(LOCCTR, LABEL, OPCODE, OPERAND)
-		count += 1
-	printSYMTAB(SYMTAB)
-	program_length = LOCCTR - starting_address
-	print('program_length = '+ intToHexStr(program_length))
-	return 
+				if(in_SYMTAB(OPERAND)):
+					if(indirect):# @m
+						continue
+					elif(immediate): # #c
+						print(OPCODE + "\t" +  OPERAND +  "\t" + intToHexStr(SYMTAB[OPERAND]))
+					elif(index):# m,x
+						continue
+					else:
+						print(OPCODE + "\t" +  OPERAND +  "\t" + intToHexStr(SYMTAB[OPERAND]))
+
+	return
 # you should call pass1 first to get the SYMTAB
 
 # print(SYMTAB)
 
 fileName = input("Please input the file name : ")
 pass1(fileName, None)
-print('======== pass1 complete ========')
+# if(pass1(fileName, None)):
+# 	pass2(fileName, None)
 
 input('Press any key to continue')
